bee6f8ad78fe95d173c5d8b2cde36e97dbc79bba
mkt918 <nagoyann1818@gmail.com>
2026-02-14 21:00:39 +0900

    chore: Add `assetPrefix` and enable `trailingSlash` in Next.js configuration for static export.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/stock_01',
+  assetPrefix: '/stock_01/',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
